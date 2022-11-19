24bed163330fa33c284c0cfb5ef7d9ea1ce429e2
Sriram <sriramsubramanian23@gmail.com>
2022-11-19 22:29:28 +0530

    Wired-up twitter user api. Made few changes in the ui

--- src/components/Tweets/tweetCard.jsx
+++ src/components/Tweets/tweetCard.jsx
@@ -1,29 +1,41 @@
 import { useEffect, useState } from "react"
+import callApi from "../axios/axios";
 
-const DP = "https://pbs.twimg.com/profile_images/1589925062659821568/IyKzZ-Af_400x400.jpg"
 const TweetCard = ({tweet}) => {
-    // TODO: Fetch username and handle from api
+    // TODO: Fetch authorname and handle from userid, instead of handle itself
+    // TODO: Code refactoring (Repetitive codes)
+    const [authorHandle, setAuthorHandle] = useState("imsriramb")
+    const [authorName, setAuthorName] = useState("")
     const [isRetweet, setRetweet] = useState(false);
-    const [userHandle, setUserHandle] = useState("imsriramb")
-    const [userName, setUserName] = useState("Sriram B")
+    const [userHandle, setUserHandle] = useState("")
+    const [userName, setUserName] = useState("")
     const [text, setText] = useState(tweet.text)
+
+    useEffect(() => {
+        callApi.get(`/twitter-user?username=${authorHandle}`).then(res => {
+            res && res.data && res.data.data && setAuthorHandle(res.data.data.username)
+            res && res.data && res.data.data && setAuthorName(res.data.data.name)
+        }).catch(err => console.error("Something went wrong: ", err))
+    }, [])
     useEffect(() => {
         const split = tweet.text && tweet.text.split(" ");
         if(split[0] === "RT") {
             setRetweet(true)
-            setUserHandle(String(split[1]).slice(1,-1))
-            setUserName(String(split[1]).slice(1,-1))
+            callApi.get(`/twitter-user?username=${String(split[1]).slice(1,-1)}`).then(res => {
+                res && res.data && res.data.data && setUserHandle(res.data.data.username)
+                res && res.data && res.data.data && setUserName(res.data.data.name)
+            }).catch(err => console.error("Something went wrong: ", err))
             const twt = split.slice(2,) 
             setText(twt.join(" "))
         }
     }, [tweet])
     return (
         <div className="tweet-card">
-            <a href={`https://twitter.com/${userHandle}/status/${tweet.id}`}>
-            {isRetweet && <span className="retweeted-by"><span className="retweet-icon">{'\u21b1'}</span><span className="retweet-inverted-icon">{'\u21b2'}</span> Retweeted by Sriram B</span>}
+            <a href={`https://twitter.com/${authorHandle}/status/${tweet.id}`}>
+            {isRetweet && <span className="retweeted-by"><span className="retweet-icon">{'\u21b1'}</span><span className="retweet-inverted-icon">{'\u21b2'}</span>&nbsp;{authorName} Retweeted</span>}
             <div className="tweet-header">
-                <a href={`https://twitter.com/${userHandle}`}><span className="tweet-user">{userName}</span></a>
-                <a href={`https://twitter.com/${userHandle}`}><span className="tweet-handle">@{userHandle}</span></a>
+                <a href={`https://twitter.com/${userHandle || authorHandle}`}><span className="tweet-user">{userName || authorName}</span></a>
+                <a href={`https://twitter.com/${userHandle || authorHandle}`}><span className="tweet-handle">@{userHandle || authorHandle}</span></a>
             </div>
             <div className="tweet-text">
                 {text}
