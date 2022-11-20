10cc4d6aee73a410a201ba349b09cfc67ca24f6b
Sriram <sriramsubramanian23@gmail.com>
2022-11-20 18:21:04 +0530

    Made the header smaller on scrolling, fixed carousel

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,8 +4,23 @@ import image from "../../assets/sriram_normal.jpg";
 import bwimage from "../../assets/sriram_bnw.jpg";
 
 export default function Header() {
+  const [isScrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+}, []);
+const handleScroll = () => {
+  if(window.pageYOffset > 250) {
+    setScrolled(true)
+  } else {
+    setScrolled(false)
+  }
+}
   return (
-    <div className="header-container">
+    <div className={`header-container ${isScrolled && 'header-scrolled'}`}>
       <h1>SR</h1>
       <h2>Sriram</h2>
     </div>
