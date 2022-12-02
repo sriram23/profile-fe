e8852f2f5bbb15a86d6b5ad5dc70a43dd346b815
Sriram <sriramsubramanian23@gmail.com>
2022-12-02 22:02:57 +0530

    Changed the base url with vercel app

--- src/components/axios/axios.jsx
+++ src/components/axios/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const callApi = axios.create({
-    baseURL: 'https://sriram-23.herokuapp.com',
+    baseURL: 'https://sriram-23.vercel.app',
   });
 
 export default callApi
